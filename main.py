7e479a946332eeaa1f3e5332e7468cebc98c5651
Michael Stallone <michael.stallone87@gmail.com>
2021-07-05 12:48:01 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# importar as bibliotecas necessárias
+import IPython
+import pandas as pd
+from pandas_datareader import data as web
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+
+
+# criar um DataFrame vazio
+df = pd.DataFrame()
